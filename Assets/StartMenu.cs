36092f7d55bde954a2d77f2729e8e766d80d6c85
RyeSingCodes <rysing03@gmail.com>
2026-06-29 13:38:41 +0800

    Add multiple enemy types and menu enhancements
    - Convert single enemy prefab to array supporting three enemy types (Rouge, SwordsMan, Tanker) for gameplay variety
    - Add scene transition fade effect to main menu
    - Adjust UI font sizes from 75 to 70 for better appearance
    - Add debug logging to play and quit button handlers
    - Update animator component IDs in character prefabs

--- Assets/StartMenu.cs
+++ Assets/StartMenu.cs
@@ -6,6 +6,9 @@ public class StartMenu : MonoBehaviour
     // This function will be triggered by your UI Play Button
     public void PlayGame()
     {
+        // Sends a message to the Unity Console terminal
+        Debug.Log("Play button clicked! Loading the next scene...");
+
         // Gets the build index of the current active scene and adds 1 to load the next one
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
@@ -13,6 +16,9 @@ public class StartMenu : MonoBehaviour
     // This function will be triggered by your UI Quit Button
     public void QuitGame()
     {
+        // Sends a message to the Unity Console terminal
+        Debug.Log("Quit button clicked! Exiting application...");
+
         // Closes the application (Only works in a standalone built game)
         Application.Quit();
     }
